c5b51fd82bdb4fe6206732ffb0b7b9b0d34934be
Luis <luis_mbp@LuisdeMacBook-Pro.local>
2025-12-12 03:25:30 +0800

    remove shits

--- alter_db.sql
+++
@@ -1,5 +0,0 @@
-USE FilmCatalog;
-
--- Add the coverUrl column if it doesn't exist
--- We use TEXT to be safe with URL lengths, though VARCHAR(255) is usually enough.
-ALTER TABLE Movies ADD COLUMN coverUrl TEXT;
